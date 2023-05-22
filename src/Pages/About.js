fa820d0db207b00d9a81fcfce4381f00baddeccf
elizabethbright721 <elizabethbright721@gmail.com>
2023-05-22 09:57:09 -0400

    update About page info

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,7 @@ const About = () => {
       
 <div>
     
-<p class="m-5  text-lg text-gray-500 md:text-xl">At Live A Better Life 24-7 our purpose is to ‘provide products and a community that you’ll feel’ and in doing that we make every life we touch change for the better. Whether your goal is to better your health, promote a healthy lifestyle, earn extra income or just give yourself an overall sense of well-being, TLC is here to help you every step of the way.</p>
+<p class="m-5  text-lg text-gray-500 md:text-xl">At Live A Better Life 24-7 (LABL247), our purpose is to ‘provide products and a community that you’ll feel’ and in doing that we make every life we touch change for the better. Whether your goal is to better your health, promote a healthy lifestyle, or just give yourself an overall sense of well-being, LABL247 is here to help you every step of the way.</p>
 <p class="m-5 text-gray-500">*These statements have not been evaluated by the Food and Drug Administration. This product is not intended to diagnose, treat, cure, or prevent any disease. Not intended for use by children. If you are pregnant, nursing, or taking medications, consult your physician prior to use.
 
 **This product is intended to support a healthy lifestyle through regular exercise and a balanced diet. Results may vary. Consult your healthcare professional about your unique medical needs before consuming any dietary supplement.</p>
